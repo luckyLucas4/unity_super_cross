e5a5f88ad91a3187673bc2ec1b83ff6d5fead8a3
luckyLucas4 <lucas.granberg00@gmail.com>
2018-04-16 15:23:40 +0200

    BikeHandler fungerar (typ)

--- Super Duper Mega Cross/Assets/Scripts/BikeHandler.cs
+++ Super Duper Mega Cross/Assets/Scripts/BikeHandler.cs
@@ -5,19 +5,33 @@ using UnityEngine;
 public class BikeHandler : MonoBehaviour {
 
     public float enginePower;
+    public float maxPower;
+    public float minPower;
+    public WheelJoint2D wheel;
 
-    public GameObject wheel;
+    private JointMotor2D m;
 
 	// Use this for initialization
 	void Start () {
-		
+        
+        m = wheel.motor;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if( Input.GetAxis("Vertical") > 0)
+        if(Input.GetAxis("Vertical") != 0 && m.motorSpeed < maxPower && m.motorSpeed > minPower)
         {
-            wheel.GetComponent<WheelJoint2D>().motor.motorSpeed = enginePower;
+            m.motorSpeed += enginePower * Input.GetAxis("Vertical");
         }
+        else if(m.motorSpeed > 0)
+        {
+            m.motorSpeed -= maxPower / 50;
+        }
+        else if(m.motorSpeed < 0)
+        {
+            m.motorSpeed -= minPower / 50;
+        }
+        wheel.motor = m;
+
 	}
 }
