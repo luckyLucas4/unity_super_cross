9b78a85e3a4aabd6434978c8a3ba0dab06b4be64
JoelSvensjo <38212440+JoelSvensjo@users.noreply.github.com>
2018-04-16 13:41:37 +0200

    Jobbar med BikeHandler

--- Super Duper Mega Cross/Assets/Scripts/BikeHandler.cs
+++ Super Duper Mega Cross/Assets/Scripts/BikeHandler.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class BikeHandler : MonoBehaviour {
 
+    public float enginePower;
+
+    public GameObject wheel;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,9 @@ public class BikeHandler : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if( Input.GetAxis("Vertical") > 0)
+        {
+            wheel.GetComponent<WheelJoint2D>().motor.motorSpeed = enginePower;
+        }
 	}
 }
