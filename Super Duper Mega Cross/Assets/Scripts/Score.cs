35b4f7e998de588932809a309c6df16ad5124863
luckyLucas4 <lucas.granberg00@gmail.com>
2018-05-14 12:55:19 +0200

    Fixade kameran

--- Super Duper Mega Cross/Assets/Scripts/Score.cs
+++ Super Duper Mega Cross/Assets/Scripts/Score.cs
@@ -4,15 +4,21 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour {
 
     public Transform player;
+    public Transform filmMan;
     public Text scoreText;
     private int maxScore = 0;
 
 	// Update is called once per frame
 	void Update () {
         int playerX = (int)player.position.x;
+        int filmManX = (int)filmMan.position.x;
         if (playerX > maxScore)
         {
             maxScore = playerX;
+        }
+        if (filmManX > maxScore)
+        {
+            maxScore = filmManX;
         }
 
         scoreText.text = maxScore.ToString("0");
