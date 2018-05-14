ca24de99885ef55bf8c59ddb9ceb2ba4b7e4feab
Johan Eriksson <38212438+JohanEriksson2000@users.noreply.github.com>
2018-05-14 12:18:17 +0200

    Visar x-koordinat

---
+++ Super Duper Mega Cross/Assets/Scripts/Score.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public Transform player;
+    public Text scoreText;
+    private int maxScore;
+
+	// Update is called once per frame
+	void Update () {
+        player.position.x.
+        scoreText.text = player.position.x.ToString("0");
+	}
+}
