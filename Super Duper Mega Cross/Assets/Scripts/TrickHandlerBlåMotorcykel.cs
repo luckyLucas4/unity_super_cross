834daf8302f98165f6c29c5670953cc9f0e67e79
luckyLucas4 <lucas.granberg00@gmail.com>
2018-05-14 13:35:38 +0200

    Merge?

--- Super Duper Mega Cross/Assets/Scripts/TrickHandlerBlåMotorcykel.cs
+++ Super Duper Mega Cross/Assets/Scripts/TrickHandlerBlåMotorcykel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrickHandlerBlå : MonoBehaviour
+public class TrickHandlerBlåMotorcykel : MonoBehaviour
 {
 
     public Sprite orginal;
