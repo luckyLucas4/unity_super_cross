777cbff951ad55dddee2e67a7998e4d1eae5e674
JoelSvensjo <38212440+JoelSvensjo@users.noreply.github.com>
2018-05-28 13:12:25 +0200

    Kommenterade kod

--- Super Duper Mega Cross/Assets/Scripts/TrickHandler.cs
+++ Super Duper Mega Cross/Assets/Scripts/TrickHandler.cs
@@ -4,37 +4,48 @@ using UnityEngine;
 
 public class TrickHandler : MonoBehaviour {
 
+    //initialiserar variablerna orginal, trick, trick2 och temp
+    
+    //De tre första variablerna är public för att de ska kunna användas i Unity
     public Sprite orginal;
     public Sprite trick;
     public Sprite trick2;
+    //Variablen "temp" är private eftersom den bara ska användas inom denna kod och inte inom Unity
     private Sprite temp;
 
 
 
-    // Use this for initialization
+    //Använder denna för initiering
     void Start () {
-        temp = orginal;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        temp = orginal; //När spelet laddas ut ska temp vara lika med original.  
+                        //Original är bunden till spriten där hunden sitter still.
+                        //Det hela gör så att den vanliga spriten används(den bild där hunden sitter still).
+    }
+
+    // Detta uppdateras varje frame/bild
+    void Update () {
 
         if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.K))
         {
-            temp = trick;
+            temp = trick; //Om "Z", "Space" eller "K" trycks ner ändras temp till att vara lika med trick
+                          //Då byts den sprite där hunden sitter still ut mot den sprite där hunden gör det första tricket 
         }
         else if (Input.GetKey(KeyCode.X) || Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.L))
         {
-            temp = trick2;
+            temp = trick2; //Om "X", "Space" eller "L" trycks ner ändras temp till att vara lika med trick
+                           //Då byts den sprite där hunden sitter still ut mot den sprite där hunden gör det andra tricket
 
-            SpriteRenderer[] ts = gameObject.GetComponentsInChildren<SpriteRenderer>();
-            ts[1].enabled = false;
-            ts[2].enabled = false;
+            //Trick 2 är det trick då motorcykeln blir till en banan. Jag var då tvungen att dölja hjulen då detta trick utförs.
+            //Då använder jag mig av SpriteRenderer och gör så att hjulens aktivering ska vara falskt och därmed döljs hjulen
+            SpriteRenderer[] wheels = gameObject.GetComponentsInChildren<SpriteRenderer>();
+            wheels[1].enabled = false;
+            wheels[2].enabled = false;
         }
-        else
+        else //Om ingen av knapparna ovan trycks ner sker det nedan
         {
-            temp = orginal;
+            temp = orginal;  //Denna rad gör så att den vanliga spriten där hunden sitter still visas 
 
+            //Eftersom hjulen ska visas när allt är i sin ursprungliga position använder jag mig av SpriteRenderer igen och sätter hjulens aktivering till sann
             SpriteRenderer[] ts = gameObject.GetComponentsInChildren<SpriteRenderer>();
             ts[1].enabled = true;
             ts[2].enabled = true;
--- Super Duper Mega Cross/Assets/Scripts/TrickHandlerBlåMotorcykel.cs
+++ Super Duper Mega Cross/Assets/Scripts/TrickHandlerBlåMotorcykel.cs
@@ -4,35 +4,42 @@ using UnityEngine;
 
 public class TrickHandlerBlåMotorcykel : MonoBehaviour
 {
+    //initialiserar variablerna orginal, trick, trick2 och temp
 
+    //De tre första variablerna är public för att de ska kunna användas i Unity
     public Sprite orginal;
     public Sprite trick;
     public Sprite trick2;
+    //Variablen "temp" är private eftersom den bara ska användas inom denna kod och inte inom Unity
     private Sprite temp;
 
 
 
-    // Use this for initialization
+    //Använder denna för initiering
     void Start()
     {
-        temp = orginal;
+        temp = orginal; //När spelet laddas ut ska temp vara lika med original.  
+                        //Original är bunden till spriten där hunden sitter still.
+                        //Det hela gör så att den vanliga spriten används(den bild där hunden sitter still).
     }
 
-    // Update is called once per frame
+    // Detta uppdateras varje frame/bild
     void Update()
     {
 
         if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.K))
         {
-            temp = trick;
+            temp = trick; //Om "Z", "Space" eller "K" trycks ner ändras temp till att vara lika med trick
+                          //Då byts den sprite där gubben sitter still ut mot den sprite där gubben gör det första tricket 
         }
         else if (Input.GetKey(KeyCode.X) || Input.GetKey(KeyCode.L))
         {
-            temp = trick2;
+            temp = trick2; //Om "X", "Space" eller "L" trycks ner ändras temp till att vara lika med trick
+                           //Då byts den sprite där gubben sitter still ut mot den sprite där gubben gör det andra tricket
         }
-        else
+        else //Om ingen av knapparna ovan trycks ner sker det nedan
         {
-            temp = orginal;
+            temp = orginal; //Denna rad gör så att den vanliga spriten där gubben sitter still visas
         }
         gameObject.GetComponent<SpriteRenderer>().sprite = temp;
     }
